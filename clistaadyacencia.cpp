2852d42af26592aa131874062d9c40e5968feb95
Ismael Avila <ismael.guada234015@gmail.com>
2021-10-22 12:46:52 -0500

    Definición e implementación de la clase cListaAdyacencia

--- clistaadyacencia.cpp
+++ clistaadyacencia.cpp
@@ -1,6 +1,71 @@
 #include "clistaadyacencia.h"
 
-cListaAdyacencia::cListaAdyacencia()
-{
+cListaAdyacencia::cListaAdyacencia() {
+    inicio.pSig = &final;
+    final.pAnt = &inicio;
+}
 
+cListaAdyacencia::~cListaAdyacencia() {
+    cIteradorLA it;
+
+    for (it=begin(); it!=end(); it++) {
+        inicio.pSig = inicio.pSig->pSig;
+        delete it.pActual;
+    }
+    final.pAnt = &inicio;
+}
+
+void cListaAdyacencia::insertar(int d, int v, int s) {
+    final.pAnt->insertarAdelante(d, v, s);
+}
+
+void cListaAdyacencia::eliminar(std::string nombre) {
+    if(isEmpty())
+        return;
+
+    cIteradorLA it;
+
+    for(it=begin(); it!=end(); it++)
+        if(it.pActual->adyacente->infoNodo.nombre == nombre)
+            it.pActual->pAnt->eliminarAdelante();
+}
+
+int cListaAdyacencia::size() {
+    int contNodes = 0;
+    cIteradorLA it;
+
+    for(it=begin(); it!=end(); it++)
+        contNodes++;
+
+    return contNodes;
+}
+
+bool cListaAdyacencia::isEmpty() {
+    return inicio.pSig == &final;
+}
+
+bool cListaAdyacencia::isFull() {
+    return false;
+}
+
+void cListaAdyacencia::imprimir() {
+    cIteradorLA it;
+
+    for(it=begin(); it!=end(); it++) {
+        std::cout << "\t\t" << it.pActual->adyacente->infoNodo.nombre << std::endl;
+        std::cout << "\tInformacion de la ruta..." << std::endl;
+        it.pActual->infoAdyacencia.imprimir();
+    }
+}
+
+cIteradorLA &cListaAdyacencia::begin() {
+    itBegin.setNodo(inicio.pSig);
+
+    return itBegin;
+}
+
+cIteradorLA &cListaAdyacencia::end() {
+    itEnd.setNodo(&final);
+
+    return itEnd;
 }
--- clistaadyacencia.h
+++ clistaadyacencia.h
@@ -1,11 +1,35 @@
 #ifndef CLISTAADYACENCIA_H
 #define CLISTAADYACENCIA_H
 
+#include "cnodola.h"
+#include "citeradorla.h"
 
-class cListaAdyacencia
-{
+class cListaAdyacencia {
+private:
+    cNodoLA inicio;
+    cNodoLA final;
+
+    cIteradorLA itBegin;
+    cIteradorLA itEnd;
+
+
 public:
     cListaAdyacencia();
+    ~cListaAdyacencia();
+
+    void insertar(int d, int v, int s); // la insercion se hace al final de la lista
+    void eliminar(std::string nombre); // la eliminacion requiere busqueda
+
+    int size();
+
+    bool isEmpty();
+    bool isFull();
+
+    void imprimir();
+
+    cIteradorLA& begin();
+    cIteradorLA& end();
+
 };
 
 #endif // CLISTAADYACENCIA_H
