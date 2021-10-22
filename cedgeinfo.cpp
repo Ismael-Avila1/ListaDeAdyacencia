e5b0ec7967fafea1ee32eb258937fb7faf50e725
Ismael Avila <ismael.guada234015@gmail.com>
2021-10-22 10:49:33 -0500

    Definición e implementación de clase cEdgeInfo

--- cedgeinfo.cpp
+++ cedgeinfo.cpp
@@ -1,6 +1,15 @@
 #include "cedgeinfo.h"
 
-cEdgeInfo::cEdgeInfo()
-{
+cEdgeInfo::cEdgeInfo() {}
 
+cEdgeInfo::cEdgeInfo(int d, int v, int s) {
+    distancia = d;
+    vista = v;
+    seguridad = s;
+}
+
+void cEdgeInfo::imprimir() {
+    std::cout << "\tDistancia: " << distancia << "km" << std::endl;
+    std::cout << "\tVista: " << vista << " puntos" << std::endl;
+    std::cout << "\tSeguridad: " << seguridad << " puntos" << std::endl;
 }
--- cedgeinfo.h
+++ cedgeinfo.h
@@ -1,11 +1,20 @@
 #ifndef CEDGEINFO_H
 #define CEDGEINFO_H
 
+#include <iostream>
 
-class cEdgeInfo
-{
-public:
+class cEdgeInfo {
+private:
+    int distancia;
+    int vista;
+    int seguridad;
+
     cEdgeInfo();
+    cEdgeInfo(int d, int v, int s);
+
+    void imprimir();
+
+    friend class cNodoLA;
 };
 
 #endif // CEDGEINFO_H
