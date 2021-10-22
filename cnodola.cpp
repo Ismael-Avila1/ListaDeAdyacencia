32301ba80ae3f15fcee648bfa606fad367870d14
Ismael Avila <ismael.guada234015@gmail.com>
2021-10-22 11:39:56 -0500

    Definición e implementación de la clase cNodoLA

--- cnodola.cpp
+++ cnodola.cpp
@@ -1,6 +1,31 @@
 #include "cnodola.h"
 
-cNodoLA::cNodoLA()
-{
+cNodoLA::cNodoLA() {
+    pAnt = nullptr;
+    pSig = nullptr;
+}
 
+cNodoLA::cNodoLA(int d, int v, int s) {
+    pAnt = nullptr;
+    pSig = nullptr;
+
+    infoAdyacencia.distancia = d;
+    infoAdyacencia.vista = v;
+    infoAdyacencia.seguridad = s;
+}
+
+void cNodoLA::insertarAdelante(int d, int v, int s) {
+    cNodoLA* newNode = new cNodoLA(d, v, s);
+
+    newNode->pSig = this->pSig;
+    newNode->pAnt = this;
+
+    this->pSig = newNode;
+    this->pSig->pSig->pAnt = newNode;
+}
+
+void cNodoLA::eliminarAdelante() {
+    this->pSig = this->pSig->pSig;
+    delete this->pSig->pAnt;
+    this->pSig->pAnt = this;
 }
--- cnodola.h
+++ cnodola.h
@@ -1,11 +1,27 @@
 #ifndef CNODOLA_H
 #define CNODOLA_H
 
+#include "cedgeinfo.h"
+#include "cnodo.h"
 
-class cNodoLA
-{
-public:
+class cNodoLA {
+private:
+    cNodoLA* pAnt;
+    cNodoLA* pSig;
+
+    cEdgeInfo infoAdyacencia;
+
+    cNodo* adyacente;
+
+
     cNodoLA();
+    cNodoLA(int d, int v, int s);
+
+    void insertarAdelante(int d, int v, int s);
+    void eliminarAdelante();
+
+    friend class cIteradorLA;
+    friend class cListaAdyacencia;
 };
 
 #endif // CNODOLA_H
