19e35c6301dd3a017ab797dc622eb050edfea711
Ismael Avila <ismael.guada234015@gmail.com>
2021-10-22 11:48:41 -0500

    Definición e implementación de la clase cIteradorLA

--- citeradorla.cpp
+++ citeradorla.cpp
@@ -1,6 +1,43 @@
 #include "citeradorla.h"
 
-cIteradorLA::cIteradorLA()
-{
+cIteradorLA::cIteradorLA() {}
 
+void cIteradorLA::setNodo(cNodoLA *pNodo) {
+    pActual = pNodo;
+}
+
+void cIteradorLA::insertarAdelante(int d, int v, int s) {
+    pActual->insertarAdelante(d, v, s);
+}
+
+void cIteradorLA::eliminarAdelante() {
+    pActual->eliminarAdelante();
+}
+
+void cIteradorLA::operator =(cIteradorLA &otro) {
+    pActual = otro.pActual;
+}
+
+bool cIteradorLA::operator ==(cIteradorLA &otro) {
+    return (pActual == otro.pActual);
+}
+
+bool cIteradorLA::operator !=(cIteradorLA &otro) {
+    return (pActual != otro.pActual);
+}
+
+void cIteradorLA::operator ++() {
+    pActual = pActual->pSig;
+}
+
+void cIteradorLA::operator --() {
+    pActual = pActual->pAnt;
+}
+
+void cIteradorLA::operator ++(int) {
+    pActual = pActual->pSig;
+}
+
+void cIteradorLA::operator --(int) {
+    pActual = pActual->pAnt;
 }
--- citeradorla.h
+++ citeradorla.h
@@ -1,11 +1,34 @@
 #ifndef CITERADORLA_H
 #define CITERADORLA_H
 
+#include <cnodola.h>
 
-class cIteradorLA
-{
+class cIteradorLA {
+private:
+    cNodoLA* pActual;
+
+    void setNodo(cNodoLA* pNodo);
+
+    friend class cListaAdyacencia;
+
+
 public:
     cIteradorLA();
+
+    void insertarAdelante(int d, int v, int s);
+    void eliminarAdelante();
+
+    // Sobrecarga de operadores
+    void operator =(cIteradorLA& otro);
+
+    bool operator ==(cIteradorLA& otro);
+    bool operator !=(cIteradorLA& otro);
+
+    void operator ++();
+    void operator --();
+
+    void operator ++(int);
+    void operator --(int);
 };
 
 #endif // CITERADORLA_H
