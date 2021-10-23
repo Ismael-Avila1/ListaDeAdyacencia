eb82e7a76be85489832b846a3bfd1b1fce0efd23
Ismael Avila <ismael.guada234015@gmail.com>
2021-10-23 14:31:12 -0500

    Definición e implementación de la clase cNodo

--- cnodo.cpp
+++ cnodo.cpp
@@ -1,6 +1,42 @@
 #include "cnodo.h"
 
-cNodo::cNodo()
-{
+cNodo::cNodo() {
+    pAnt = nullptr;
+    pSig = nullptr;
+}
 
+cNodo::cNodo(std::string nombre, std::string estado, int magicoDesde, std::string clima, int temp, std::string atraccion) {
+    pAnt = nullptr;
+    pSig = nullptr;
+
+    infoNodo.nombre = nombre;
+    infoNodo.estado = estado;
+    infoNodo.MagicoDesde = magicoDesde;
+    infoNodo.tipoClima = clima;
+    infoNodo.tempMedia = temp;
+    infoNodo.atraccPrincipal = atraccion;
+}
+
+void cNodo::insertarAdelante(std::string nombre, std::string estado, int magicoDesde, std::string clima, int temp, std::string atraccion) {
+    cNodo* newNode = new cNodo(nombre, estado, magicoDesde, clima, temp, atraccion);
+
+    newNode->pSig = this->pSig;
+    newNode->pAnt = this;
+
+    this->pSig = newNode;
+    this->pSig->pSig->pAnt = this;
+}
+
+void cNodo::insertarAdyacencia(int d, int v, int s, std::string a) {
+    this->lstLA.insertar(d, v, s, a);
+}
+
+void cNodo::eliminarAdelante() {
+    this->pSig = this->pSig->pSig;
+    delete this->pSig->pAnt;
+    this->pSig->pAnt = this;
+}
+
+void cNodo::eliminarAdyacencia(std::string nombre) {
+    this->lstLA.eliminar(nombre);
 }
--- cnodo.h
+++ cnodo.h
@@ -1,11 +1,28 @@
 #ifndef CNODO_H
 #define CNODO_H
 
+#include "cinfopueblo.h"
+#include "clistaadyacencia.h"
 
-class cNodo
-{
-public:
+class cNodo {
+private:
+    cNodo* pAnt;
+    cNodo* pSig;
+
+    cInfoPueblo infoNodo;
+    cListaAdyacencia lstLA;
+
     cNodo();
+    cNodo(std::string nombre, std::string estado, int magicoDesde, std::string clima, int temp, std::string atraccion);
+
+    void insertarAdelante(std::string nombre, std::string estado, int magicoDesde, std::string clima, int temp, std::string atraccion); // Inserta nodo en la lista de nodos del grafo
+    void insertarAdyacencia(int d, int v, int s, std::string a); // Inserta adyacencia en su lista de adyacencias
+
+    void eliminarAdelante();    // Elimina nodo en la lista de nodos del grafo
+    void eliminarAdyacencia(std::string nombre);  // Elimina adyacencia en su lista de adyacencia
+
+    friend class cIterador;
+    friend class cLista;
 };
 
 #endif // CNODO_H
