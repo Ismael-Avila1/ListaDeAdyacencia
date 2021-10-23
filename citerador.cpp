2dcaa1cd1ec15e05ed563172f14b4354b18a3139
Ismael Avila <ismael.guada234015@gmail.com>
2021-10-23 14:40:50 -0500

    Definición e implementación de la clase cIterador

--- citerador.cpp
+++ citerador.cpp
@@ -1,6 +1,51 @@
 #include "citerador.h"
 
-cIterador::cIterador()
-{
+cIterador::cIterador() {}
 
+void cIterador::setNodo(cNodo *pNodo) {
+    pActual = pNodo;
+}
+
+void cIterador::insertarAdelante(std::string nombre, std::string estado, int magicoDesde, std::string clima, int temp, std::string atraccion) {
+    pActual->insertarAdelante(nombre, estado, magicoDesde, clima, temp, atraccion);
+}
+
+void cIterador::insertarAdyacencia(int d, int v, int s, std::string a) {
+    pActual->insertarAdyacencia(d, v, s, a);
+}
+
+void cIterador::eliminarAdelante() {
+    pActual->eliminarAdelante();
+}
+
+void cIterador::eliminarAdyacencia(std::string nombre) {
+    pActual->eliminarAdyacencia(nombre);
+}
+
+void cIterador::operator =(cIterador &otro) {
+    pActual = otro.pActual;
+}
+
+bool cIterador::operator ==(cIterador &otro) {
+    return (pActual == otro.pActual);
+}
+
+bool cIterador::operator !=(cIterador &otro) {
+    return (pActual != otro.pActual);
+}
+
+void cIterador::operator ++() {
+    pActual = pActual->pSig;
+}
+
+void cIterador::operator --() {
+    pActual = pActual->pAnt;
+}
+
+void cIterador::operator ++(int) {
+    pActual = pActual->pSig;
+}
+
+void cIterador::operator --(int) {
+    pActual = pActual->pAnt;
 }
--- citerador.h
+++ citerador.h
@@ -1,11 +1,37 @@
 #ifndef CITERADOR_H
 #define CITERADOR_H
 
+#include "cnodo.h"
 
-class cIterador
-{
+class cIterador {
+private:
+    cNodo* pActual;
+
+    void setNodo(cNodo* pNodo);
+
+    friend class cLista;
+
+
 public:
     cIterador();
+
+    void insertarAdelante(std::string nombre, std::string estado, int magicoDesde, std::string clima, int temp, std::string atraccion);
+    void insertarAdyacencia(int d, int v, int s, std::string a);
+
+    void eliminarAdelante();
+    void eliminarAdyacencia(std::string nombre);
+
+    // Sobrecarga de operadores
+    void operator =(cIterador& otro);
+
+    bool operator ==(cIterador& otro);
+    bool operator !=(cIterador& otro);
+
+    void operator ++();
+    void operator --();
+
+    void operator ++(int);
+    void operator --(int);
 };
 
 #endif // CITERADOR_H
