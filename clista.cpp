d533594907c5a270538238d038182f472a4dbefc
Ismael Avila <ismael.guada234015@gmail.com>
2021-10-24 15:06:51 -0500

    Definición e implementación de la clase cLista

--- clista.cpp
+++ clista.cpp
@@ -1,6 +1,113 @@
 #include "clista.h"
 
-cLista::cLista()
-{
+cLista::cLista() {}
 
+cLista::~cLista() {
+    cIterador it;
+
+    for(it=begin(); it!=end(); it++) {
+        inicio.pSig = inicio.pSig->pSig;
+        it.pActual->lstLA.~cListaAdyacencia();
+        delete it.pActual;
+    }
+    final.pAnt = &inicio;
+}
+
+cNodo *cLista::getNodo(std::string nombreNodo) {
+    cNodo* temp = inicio.pSig;
+
+    while (temp != &final)
+        if(temp->infoNodo.nombre == nombreNodo)
+            return temp;
+
+    return nullptr;
+}
+
+void cLista::insertarNodo(std::string nombre, std::string estado, int magicoDesde, std::string clima, int temp, std::string atraccion) {
+    final.pAnt->insertarAdelante(nombre, estado, magicoDesde, clima, temp, atraccion);
+}
+
+void cLista::insertarAdyacencia(std::string origen, int d, int v, int s, std::string destino) {
+    cNodo* pOrigen = getNodo(origen);
+    cNodo* pDestino = getNodo(destino);
+
+    if(pOrigen == nullptr or pDestino == nullptr) {
+        std::cout << "Nodos de origen o destino incorrectos" << std::endl;
+        return;
+    }
+
+    pOrigen->lstLA.insertar(d, v, s, destino);
+    pDestino->lstLA.insertar(d, v, s, origen);
+}
+
+void cLista::eliminarNodo(std::string nombre) {
+    cIterador it;
+
+    for(it=begin(); it!=end(); it++)
+        if(it.pActual->infoNodo.nombre == nombre) {
+
+            cIterador it2;
+            for(it2=begin(); it2!=end(); it++)
+                it2.pActual->lstLA.eliminar(nombre);
+
+            it.pActual->pAnt->eliminarAdelante();
+        }
+}
+
+void cLista::eliminarAdyacencia(std::string origen, std::string destino) {
+    cNodo* pOrigen = getNodo(origen);
+    cNodo* pDestino = getNodo(destino);
+
+    if(pOrigen == nullptr or pDestino == nullptr) {
+        std::cout << "Nodos de origen o destino incorrectos" << std::endl;
+        return;
+    }
+
+    pOrigen->lstLA.eliminar(destino);
+    pDestino->lstLA.eliminar(origen);
+}
+
+int cLista::size() {
+    int contNodes = 0;
+    cIterador it;
+
+    for(it=begin(); it!=end(); it++)
+        contNodes++;
+
+    return contNodes;
+}
+
+bool cLista::isEmpty() {
+    return inicio.pSig == &final;
+}
+
+bool cLista::isFull() {
+    return false;
+}
+
+void cLista::imprimir() {
+    if(isEmpty()) {
+        std::cout << std::endl << "Grafo Vacio" << std::endl;
+        return;
+    }
+
+    cIterador it;
+    for(it=begin(); it!=end(); it++) {
+        it.pActual->infoNodo.imprimir();
+
+        std::cout << std::endl << "\tDe " << it.pActual->infoNodo.nombre << " puedes ir a..." << std::endl << std::endl;
+        it.pActual->lstLA.imprimir();
+    }
+}
+
+cIterador &cLista::begin() {
+    itBegin.setNodo(inicio.pSig);
+
+    return itBegin;
+}
+
+cIterador &cLista::end() {
+    itEnd.setNodo(&final);
+
+    return itEnd;
 }
--- clista.h
+++ clista.h
@@ -1,11 +1,39 @@
 #ifndef CLISTA_H
 #define CLISTA_H
 
+#include "cnodo.h"
+#include "citerador.h"
 
-class cLista
-{
+class cLista {
+private:
+    cNodo inicio;
+    cNodo final;
+
+    cIterador itBegin;
+    cIterador itEnd;
+
+    cNodo* getNodo(std::string nombreNodo);
+
+
 public:
     cLista();
+    ~cLista();
+
+    void insertarNodo(std::string nombre, std::string estado, int magicoDesde, std::string clima, int temp, std::string atraccion); // La insercion se hace al final de la lista
+    void insertarAdyacencia(std::string origen, int d, int v, int s, std::string destino); // Inserta adyacencia en la lista de adyacencias de un nodo. Requeire busqueda
+
+    void eliminarNodo(std::string nombre); // Elimina nodo en la lista de nodos del grafo. Requiere busqueda
+    void eliminarAdyacencia(std::string origen, std::string destino);
+
+    int size();
+
+    bool isEmpty();
+    bool isFull();
+
+    void imprimir();
+
+    cIterador& begin();
+    cIterador& end();
 };
 
 #endif // CLISTA_H
